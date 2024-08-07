0ac88432d77ea450f2c0a2d5d4875128ebaba373
vajid hussain k <vajid.hussain@bootlabstech.com>
2024-08-07 17:57:52 +0530

    test1 branch first commit

--- main.go
+++ main.go
@@ -3,60 +3,62 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"jsonTest/db"
 	"jsonTest/model"
-	"log"
-	"net/http"
 	"os"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 func main() {
 
-	gitFile, err := http.Get("https://raw.githubusercontent.com/vajid-hussain7/terraformPlan/main/terraform-plan.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	CreateCommit()
 
-	gitFileData, err := io.ReadAll(gitFile.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var json interface{}
-	var MainField map[string]interface{}
-
-	// extract data from json
-
-	// file, err := ExtractFile()
+	// gitFile, err := http.Get("https://raw.githubusercontent.com/vajid-hussain7/terraformPlan/main/terraform-plan.json")
 	// if err != nil {
-	// 	log.Fatalln("file reading lead to error ", err)
+	// 	log.Fatalln(err)
 	// }
-	// create and write the terraform plan data to plan.json file
-	err = os.WriteFile("plan.json", gitFileData, os.FileMode(0744))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	fmt.Println("created")
+	// gitFileData, err := io.ReadAll(gitFile.Body)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	// unmarshel file
-	err = Unmarshel(gitFileData, &json)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// var json interface{}
+	// var MainField map[string]interface{}
 
-	// create the json result for storing in to db
-	MainField = json.(map[string]interface{})
-	result := FinalResult(MainField["resource_changes"].([]interface{}))
+	// // extract data from json
 
-	err = db.InitDB()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // file, err := ExtractFile()
+	// // if err != nil {
+	// // 	log.Fatalln("file reading lead to error ", err)
+	// // }
+	// // create and write the terraform plan data to plan.json file
+	// err = os.WriteFile("plan.json", gitFileData, os.FileMode(0744))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	db.InsertResourse(result)
+	// fmt.Println("created")
+
+	// // unmarshel file
+	// err = Unmarshel(gitFileData, &json)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// // create the json result for storing in to db
+	// MainField = json.(map[string]interface{})
+	// result := FinalResult(MainField["resource_changes"].([]interface{}))
+
+	// err = db.InitDB()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// db.InsertResourse(result)
 
 }
 
@@ -88,7 +90,55 @@ func FinalResult(resousrseChange []interface{}) model.ResourseChanges {
 	return finalResult
 }
 
+func CreateCommit() {
 
-func CreateCommit(){
-	
+	localRepo, err := git.PlainOpen("./")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// _, err = localRepo.Branch("test")
+	// if err != nil {
+	// 	err := localRepo.CreateBranch(&config.Branch{Name: "test"})
+	// 	if err != nil {
+	// 		fmt.Println("create a branch ", err)
+	// 	}
+	// } else {
+	// 	fmt.Println(err)
+	// }
+
+	workTree, err := localRepo.Worktree()
+	if err != nil {
+		fmt.Println("from worktree", err)
+	}
+
+	branch := plumbing.NewBranchReferenceName("test1")
+
+	err = workTree.Checkout(&git.CheckoutOptions{Branch: branch, Create: true})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = workTree.Add("./")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = workTree.Commit("test1 branch first commit ", &git.CommitOptions{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var auth = &http.BasicAuth{
+		Username: "vajid-hussain7",
+		Password: os.Getenv("git_token"),
+	}
+
+	err = localRepo.Push(&git.PushOptions{RemoteName: "origin", Auth: auth, Force: true})
+	if err != nil {
+		fmt.Println(err)
+	}
+
 }
+
+// ErrBranchExists
